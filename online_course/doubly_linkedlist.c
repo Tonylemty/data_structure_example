ddd9a23785a78ce24f879ce9f1e14bb3be1d2e87
Tonylemty <chengshinchung31@gmail.com>
2023-08-27 20:38:21 +0800

    Data Structure full course notes

---
+++ online_course/doubly_linkedlist.c
@@ -0,0 +1,63 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* head; // global variable - pointer to head node.
+
+struct Node* GetNewNode(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+void InsertAtHead(int x) {
+    struct Node* newNode = GetNewNode(x);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    head->prev = newNode;
+    newNode->next = head;
+    head = newNode;
+    
+}
+
+void Print() {
+    struct Node* temp = head;
+    printf("Forward: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void ReversePrint() {
+    struct Node* temp = head;
+    if (temp == NULL) {
+        return;
+    }
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    printf("Reverse: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->prev;
+    }
+}
+
+int main() {
+    head = NULL; //empty list.
+    InsertAtHead(2); Print(); ReversePrint();
+    InsertAtHead(4); Print(); ReversePrint();
+    InsertAtHead(6); Print(); ReversePrint();    
+}
